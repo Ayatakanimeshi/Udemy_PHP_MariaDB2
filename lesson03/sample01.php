a676d7486361d7eacf57342805f0edaa88ce9b41
Ayatakanimeshi <ayatakathebattler2top@gmail.com>
2023-11-15 20:36:40 +0900

    lesson2

--- lesson03/sample01.php
+++ lesson03/sample01.php
@@ -8,7 +8,13 @@
 <body>
     <?php
     $db = new mysqli('127.0.0.1:8889', 'root', 'root', 'mydb');
-    echo 'DBに接続しました';
+    $db->query('drop table if exists test');
+    $success = $db->query('create table test(id INT)');
+    if ($success) {
+    echo 'テーブルを削除して作成しました';
+    } else {
+        echo '正常に動作しませんでした';
+    }
     ?>
 </body>
 </html>
