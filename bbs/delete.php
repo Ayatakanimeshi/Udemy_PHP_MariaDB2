305be78513b6687c43232828c2cbb7beb9acd675
Ayatakanimeshi <ayatakathebattler2top@gmail.com>
2023-11-29 12:16:31 +0900

    削除画面

--- bbs/delete.php
+++ bbs/delete.php
@@ -1,3 +1,31 @@
 <?php
+session_start();
+require('library.php');
+
+if (isset($_SESSION['id']) && isset($_SESSION['name'])) {
+    $id = $_SESSION['id'];
+    $name = $_SESSION['name'];
+} else {
+    header('Location: login.php');
+    exit();
+}
+
+$post_id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+if (!$post_id){
+    header('Locatilon: index.php');
+    exit();
+}
+
+$db = dbconnect();
+$stmt = $db->prepare('delete from posts where id=? and member_id=? limit 1');
+if (!$stmt) {
+    die($db->error);
+}
+$stmt->bind_param('i', $post_id, $id);
+$success = $stmt->execute();
+if (!$success){
+    die($db->error);
+}
+
 header('Location: index.php'); exit();
 ?>
